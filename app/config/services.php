229928de917ee69558d1723ce14cb53958f45393
Luke Hardiman <luke@hardiman.co.nz>
2015-08-11 22:17:07 +1200

    updated db
    updated db to use config

--- app/config/services.php
+++ app/config/services.php
@@ -60,12 +60,12 @@ $di->set('view', function () use ($config) {
 /**
  * Database connection is created based in the parameters defined in the configuration file
  */
-$di->set('db', function(){
+$di->set('db', function() use ($config){
     return new DbAdapter(array(
-        'host'        => 'localhost',
-        'username'    => 'airport_taxi',
-        'password'    => 'airport_taxi468',
-        'dbname'      => 'airport_taxi',
+        'host'        => $config->database->host,
+        'username'    => $config->database->username,
+        'password'    => $config->database->password,
+        'dbname'      => $config->database->dbname,
     ));
 });
 /*
--- app/views/index.volt
+++ app/views/index.volt
@@ -3,7 +3,7 @@
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Phalcon PHP Framework</title>
+        <title>Taxi System</title>
     </head>
     <body>
         {{ content() }}
